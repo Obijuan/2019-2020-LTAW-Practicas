36fbfabccc83b92a9d819894dffc59765ab9ee39
Obijuan <juan@iearobotics.com>
2020-02-20 18:40:40 +0100

    P2: database updated

--- P2/mi_proyectoweb/mi_tienda/models.py
+++ P2/mi_proyectoweb/mi_tienda/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    """Modelo de datos de mis productos"""
+
+    nombre = models.CharField(max_length=50)
+    stock = models.IntegerField(default=0)
+    precio = models.FloatField()
+
+    # -- Usamos el nombre para identificar
+    # -- el producto
+    def __str__(self):
+        return self.nombre
