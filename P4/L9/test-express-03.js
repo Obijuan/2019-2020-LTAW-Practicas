29c3e7a7b4718cb5240733b0c8ca945523b3559f
Obijuan <juan@iearobotics.com>
2020-03-21 13:27:21 +0100

    P4: L9: Example 3 added

---
+++ P4/L9/test-express-03.js
@@ -0,0 +1,32 @@
+//-- Cargar las dependencias
+//-- Modulo express
+const express = require('express')
+
+//-- Crear una nueva aplciacion web
+const app = express()
+
+//-- Crear un servidor. Los mensajes recibidos
+//-- los gestiona la app
+const http = require('http').Server(app);
+
+//-- Puerto donde lanzar el servidor
+const PORT = 8080
+
+//-- Lanzar servidor
+http.listen(PORT, function(){
+  console.log('Servidor lanzado en puerto ' + PORT);
+});
+
+//-------- PUNTOS DE ENTRADA DE LA APLICACION WEB
+//-- Página principal
+app.get('/', (req, res) => {
+  let fich = __dirname + '/index.html';
+  res.sendFile(fich);
+  console.log("Acceso a " + fich);
+});
+
+//-- Otra vista
+app.get('/woala', (req, res) => {
+  res.send('WOALA! Chuck Norris approved!! :-)');
+  console.log("Acceso a /woala");
+});
