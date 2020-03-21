e31e04c09d00d9006e5dc8d2a704b410b52a8ff3
Obijuan <juan@iearobotics.com>
2020-03-21 18:01:08 +0100

    P4: L9: chat-1 added

---
+++ P4/L9/chat-1-server.js
@@ -0,0 +1,44 @@
+//-- Cargar las dependencias
+//-- Modulo express
+const express = require('express')
+
+//-- Crear una nueva aplciacion web
+const app = express()
+
+//-- Crear un servidor. Los mensajes recibidos
+//-- los gestiona la app
+const http = require('http').Server(app);
+
+//-- Biblioteca socket.io en el lado del servidor
+const io = require('socket.io')(http);
+
+//-- Puerto donde lanzar el servidor
+const PORT = 8080
+
+function servir_fichero(fich, res)
+{
+  let path = __dirname + '/' + fich;
+  res.sendFile(path);
+  console.log("Acceso a " + path);
+}
+
+//-- Lanzar servidor
+http.listen(PORT, function(){
+  console.log('Servidor lanzado en puerto ' + PORT);
+});
+
+//-------- PUNTOS DE ENTRADA DE LA APLICACION WEB
+//-- Página principal
+app.get('/', (req, res) => {
+  servir_fichero("chat-1.html", res)
+});
+
+//-- Otra vista de prueba
+app.get('/woala', (req, res) => {
+  res.send('WOALA! Chuck Norris approved!! :-)');
+  console.log("Acceso a /woala");
+});
+
+//-- El resto de peticiones se interpretan como
+//-- ficheros estáticos
+app.use('/', express.static(__dirname +'/'));
