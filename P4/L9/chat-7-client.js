637a7b09ac74d2902a1b7a1de79bc08857a6e303
Obijuan <juan@iearobotics.com>
2020-03-22 12:20:51 +0100

    P4: L9: Chat-7 added

---
+++ P4/L9/chat-7-client.js
@@ -0,0 +1,34 @@
+console.log("Ejecutando cliente JS...");
+
+//-- Obtener los elementos del DOM
+const display = document.getElementById("display");
+const msg = document.getElementById("msg");
+const send = document.getElementById("send");
+
+//-- Crear un websocket. Se establece la conexión con el servidor
+const socket = io();
+
+//-- Se ha recibido el evento 'hello':
+//-- Es el mensaje de bienvenida del servidor
+socket.on('hello', (msg) => {
+
+  //-- Mostrarlo en la consola del navegador, para
+  //-- depurar
+  console.log("Mensaje del servidor: " + msg);
+
+  //-- Ponerlo en el párrafo display
+  display.innerHTML = msg;
+});
+
+//-- Botón de envío apretado
+send.onclick = () => {
+
+  //-- Se envía el mensaje escrito
+  //-- Usamos el nombre 'msg' para los mensajes de usuario
+  //-- Si no se ha introducido ningún mensaje, no se envía
+  if (msg.value)
+    socket.emit('msg', msg.value)
+
+  //-- Borramos el mensaje escrito
+  msg.value="";
+}
